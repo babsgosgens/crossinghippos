34c7e086cb1b592a7207a8c8a69866b88f65ccad
Jean Brouwers <jeanbrouwers@outlook.com>
2013-09-19 15:45:33 +0200

    Added default flickr template

--- components/com_streams/views/streams/tmpl/default_flickr.php
+++ components/com_streams/views/streams/tmpl/default_flickr.php
@@ -15,4 +15,9 @@ $post = $this->post;
 // echo '<pre>';
 // print_r($post);
 // echo '</pre>';
+// exit;
 ?>
+
+	<a href="<?php echo $post->owner->nsid; ?>"><span><?php echo $post->owner->username; ?></span></a>
+	<a href="<?php echo $post->urls->url[0]->_content; ?>"><span><?php echo $post->title->_content; ?></span>
+	<img src="<?php echo 'http://farm'.$post->farm.'.staticflickr.com/'.$post->server.'/'.$post->id.'_'.$post->secret.'_m.jpg'; ?>" width="150"></a>
