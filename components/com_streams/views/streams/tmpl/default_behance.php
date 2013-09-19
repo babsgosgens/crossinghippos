75df6a4aaebc29df1be92fa176be17e843e885c8
Jean Brouwers <jeanbrouwers@outlook.com>
2013-09-19 15:46:07 +0200

    Finalized behance template

---
+++ components/com_streams/views/streams/tmpl/default_behance.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_weblinks
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$post = $this->post;
+
+// Uncomment for available attributes
+// echo '<pre>';
+// print_r($post);
+// echo '</pre>';
+// exit;
+?>
+
+	<img src="<?php echo $post->owners[0]->images->{50}; ?>" width="32" height="32">
+	<a href="<?php echo $post->owners[0]->url; ?>"><span><?php echo $post->owners[0]->username; ?></span></a>
+	<a href="<?php echo $post->url; ?>"><span><?php echo $post->name; ?></span>
+	<img src="<?php echo $post->covers->{202}; ?>" width="150"></a>
