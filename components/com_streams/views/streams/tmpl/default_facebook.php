3f8cf425ef0a02e5e92ca0bf63f895ba82fec665
Jean Brouwers <jeanbrouwers@outlook.com>
2013-10-01 13:08:42 +0200

    Fixed dead image facebook

--- components/com_streams/views/streams/tmpl/default_facebook.php
+++ components/com_streams/views/streams/tmpl/default_facebook.php
@@ -20,8 +20,14 @@ $post = $this->post;
 $patterns = array("/(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))/", '/@(\w+)/', '/#(\w+)/');
 $replace = array('<a href="$1">$1</a>', '<a href="https://www.facebook.com/$1">@$1</a>&nbsp;', '<a href="https://www.facebook.com/hashtag/$1">#$1</a>');
 $formatted_post = preg_replace($patterns, $replace, $post->message);
+
+if ($post->picture != ''){
+	$picture = '<p><img src="' . $post->picture . '" /></p>';
+} else {
+	$picture = '';
+}
 ?>
 
 <a href="https://www.facebook.com/<?php echo $post->from->id; ?>"><span><?php echo $post->from->name;?></span></a>
 <p><?php echo $formatted_post; ?></p>
-<p><img src="<?php echo $post->picture ?>" /></p>
+<?php echo $picture; ?>
