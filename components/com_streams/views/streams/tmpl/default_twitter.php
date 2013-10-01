fbeb923454f7e90683f5cd375c328f4b37eaeb70
Jean Brouwers <jeanbrouwers@outlook.com>
2013-10-01 12:58:23 +0200

    Fixed template errors

--- components/com_streams/views/streams/tmpl/default_twitter.php
+++ components/com_streams/views/streams/tmpl/default_twitter.php
@@ -10,27 +10,19 @@
 defined('_JEXEC') or die;
 
 $post = $this->post;
-$user = $post->user;
-/*
-	// Call attributes like so
-	$created_at = $post->created_at;
-	$text = $post->text;
- */
 
 // Uncomment for available attributes
-echo '<pre>';
-// print_r($user);
+// echo '<pre>';
 // print_r($post);
-echo '</pre>';
+// echo '</pre>';
 
 // Format the content
 $patterns = array("/(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))/", '/@(\w+)/', '/#(\w+)/');
-$replace = array('<a href="$1">$1</a>', '<a href="https://www.twitter.com/$1">@$1</a>&nbsp;', '<a href="https://twitter.com/search?q=%23$1&src=hash">#$1</a>');
+$replace = array('<a href="$1">$1</a>', '<a href="https://www.twitter.com/$1">@$1</a>', '<a href="https://twitter.com/search?q=%23$1&src=hash">#$1</a>');
 $formatted_post = preg_replace($patterns, $replace, $post->text);
-
 ?>
 
-	<img src="<?php echo $user->profile_image_url; ?>" width="32" height="32">
-	<!-- DON'T FORGET TO REMOVE EMBEDDED EM STYLES -->
-	<a href="https://www.twitter.com/<?php echo $user->screen_name; ?>"><span><?php echo $user->name;?></span><em>@<?php echo $user->screen_name;?></em></a>
-	<p><?php echo $formatted_post; ?></p>
+<img src="<?php echo $post->user->profile_image_url; ?>" width="32" height="32">
+<!-- DON'T FORGET TO REMOVE EMBEDDED EM STYLES -->
+<a href="https://www.twitter.com/<?php echo $post->user->screen_name; ?>"><span><?php echo $post->user->name;?></span><em>@<?php echo $post->user->screen_name;?></em></a>
+<p><?php echo $formatted_post; ?></p>
