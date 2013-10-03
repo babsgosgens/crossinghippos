c17a0238536e5ddc32726d4416e22dfcd2562191
Jean Brouwers <jeanbrouwers@outlook.com>
2013-10-03 14:12:01 +0200

    Google+ url support

--- components/com_streams/views/streams/tmpl/default_googleplus.php
+++ components/com_streams/views/streams/tmpl/default_googleplus.php
@@ -24,11 +24,15 @@ $post = $this->post;
 <!-- ITEM -->
 <?php
 if (isset($post['object']['attachments'][0])){
-	// if photo
 	if ($post['object']['attachments'][0]['objectType'] == 'photo'){
 		echo '<a href="' . $post['url'] . '"><h1>' . $post['title'] . '</h1><img src="' . $post['object']['attachments'][0]['image']['url'] . '"></img></a>';
 	} elseif ($post['object']['attachments'][0]['objectType'] == 'video'){
-		echo '<h1>' . $post['title'] . '</h1><iframe src="' . strtok($post['object']['attachments'][0]['embed']['url'], '?') . '?feature=player_embedded&autoplay=0" frameborder="0" allowfullscreen></iframe>';
+		echo '<h1>' . $post['object']['attachments'][0]['displayName'] . '</h1>';
+		echo '<iframe width="100%" height="100%" src="' . strtok($post['object']['attachments'][0]['embed']['url'], '?') . '?feature=player_embedded&autoplay=0" frameborder="0" allowfullscreen></iframe>';
+	} elseif ($post['object']['attachments'][0]['objectType'] == 'article'){
+		echo '<h1>' . $post['object']['attachments'][0]['displayName'] . '</h1>';
+		echo '<img src="' . $post['object']['attachments'][0]['image']['url'] . '">';
+		echo '<a href="' . $post['object']['attachments'][0]['url'] . '">' . $post['object']['attachments'][0]['url'] . '</a>';
 	}
 }
 ?>
