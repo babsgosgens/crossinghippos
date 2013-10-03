51d83425d1461631ad1cfc11b93c636953a37ae8
Jean Brouwers <jeanbrouwers@outlook.com>
2013-10-03 14:19:41 +0200

    Google + fixed

--- components/com_streams/views/streams/tmpl/default_googleplus.php
+++ components/com_streams/views/streams/tmpl/default_googleplus.php
@@ -20,19 +20,32 @@ $post = $this->post;
 <!-- PROFILE -->
 <?php echo '<img src="' . $post['actor']['image']['url'] . '"></img>'; ?>
 <?php echo $post['actor']['displayName']; ?>
+<!-- PROFILE -->
+
+<!-- TITLE -->
+<?php echo '<h1>' . $post['title'] .'</h1>'; ?>
+<!-- TITLE -->
 
 <!-- ITEM -->
 <?php
 if (isset($post['object']['attachments'][0])){
 	if ($post['object']['attachments'][0]['objectType'] == 'photo'){
-		echo '<a href="' . $post['url'] . '"><h1>' . $post['title'] . '</h1><img src="' . $post['object']['attachments'][0]['image']['url'] . '"></img></a>';
+		/*
+		* PHOTOS
+		*/
+		echo '<a href="' . $post['url'] . '"><img src="' . $post['object']['attachments'][0]['image']['url'] . '"></img></a>';
 	} elseif ($post['object']['attachments'][0]['objectType'] == 'video'){
-		echo '<h1>' . $post['object']['attachments'][0]['displayName'] . '</h1>';
+		/*
+		* VIDEOS
+		*/
 		echo '<iframe width="100%" height="100%" src="' . strtok($post['object']['attachments'][0]['embed']['url'], '?') . '?feature=player_embedded&autoplay=0" frameborder="0" allowfullscreen></iframe>';
 	} elseif ($post['object']['attachments'][0]['objectType'] == 'article'){
-		echo '<h1>' . $post['object']['attachments'][0]['displayName'] . '</h1>';
+		/*
+		* LINKS
+		*/
 		echo '<img src="' . $post['object']['attachments'][0]['image']['url'] . '">';
 		echo '<a href="' . $post['object']['attachments'][0]['url'] . '">' . $post['object']['attachments'][0]['url'] . '</a>';
 	}
 }
 ?>
+<!-- ITEM -->
