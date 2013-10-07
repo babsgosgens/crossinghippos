dad524e6b361d7f1a50f83bda1188f68e7c78b25
JvG94 <jellevangrinsven@gmail.com>
2013-10-07 10:04:44 +0200

    Changed facebook plugin

--- components/com_streams/views/streams/tmpl/default_facebook.php
+++ components/com_streams/views/streams/tmpl/default_facebook.php
@@ -17,17 +17,24 @@ $post = $this->post;
 // echo '</pre>';
 
 // Format the content
-$patterns = array("/(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))/", '/@(\w+)/', '/#(\w+)/');
-$replace = array('<a href="$1">$1</a>', '<a href="https://www.facebook.com/$1">@$1</a>&nbsp;', '<a href="https://www.facebook.com/hashtag/$1">#$1</a>');
-$formatted_post = preg_replace($patterns, $replace, $post->message);
-
-if (isset($post->picture)){
-	$picture = '<p><img src="' . $post->picture . '"></p>';
-} else {
-	$picture = '';
+if (isset($post->message))
+{
+	$patterns = array("/(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))/", '/@(\w+)/', '/#(\w+)/');
+	$replace = array('<a href="$1">$1</a>', '<a href="https://www.facebook.com/$1">@$1</a>&nbsp;', '<a href="https://www.facebook.com/hashtag/$1">#$1</a>');
+	$formatted_post = preg_replace($patterns, $replace, $post->message);
 }
 ?>
 
 <a href="https://www.facebook.com/<?php echo $post->from->id; ?>"><span><?php echo $post->from->name;?></span></a>
-<p><?php echo $formatted_post; ?></p>
-<?php echo $picture; ?>
+<?php
+if (isset($formatted_post))
+{
+	echo '<p>' . $formatted_post . '</p>';
+}
+
+if (isset($post->picture))
+{
+	echo '<p><img src="' . $post->picture . '"></p>';
+}
+
+?>
