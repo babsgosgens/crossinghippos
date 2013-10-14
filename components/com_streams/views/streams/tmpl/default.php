db3ba538e8d747f1b61c5b89662460fe138697be
Jean Brouwers <jeanbrouwers@outlook.com>
2013-10-14 14:07:48 +0200

    Went to 2 items slide

--- components/com_streams/views/streams/tmpl/default.php
+++ components/com_streams/views/streams/tmpl/default.php
@@ -12,60 +12,20 @@ require_once( JPATH_SITE.'/libraries/mobiledetect/Mobile_Detect.php' );
 $detect = new Mobile_Detect;
 ?>
 
+<article class="stream intro">
+	<p>
+		Hello, this is a small intro block.
+		I describe unnecessery info here.
+		And this is also not interesting.
+	</p>
+</article>
+
 <div class="scrollable" id="scrollable">
   	<div class="scrollable__items">
-	<?php if(!$detect->isMobile()): ?>
-
-		<div>
-			  	<article class="stream intro">
-			  		<p>
-			  			Hello, this is a small intro block.
-			  			I describe unnecessery info here.
-			  			And this is also not interesting.
-			  		</p>
-			  	</article>
-
-			  	<?php
-			  	$date = new JDate($this->items[0]->date_created);
-				$this->items[0]->date_created = $date->format(JText::_('DATE_FORMAT_LC2'));
-				$platform = $this->items[0]->platform;
-				$this->post = $this->items[0]->php;
-				?>
-
-			  	<article class="stream <?php echo $this->items[0]->platform; ?>">
-		  		<?php echo $this->loadTemplate($this->items[0]->platform); ?>
-		  		<time><?php echo $this->items[0]->date_created; ?></time>
-		  		</article>
-
-		  		<?php
-			  	$date = new JDate($this->items[1]->date_created);
-				$this->items[1]->date_created = $date->format(JText::_('DATE_FORMAT_LC2'));
-				$platform = $this->items[1]->platform;
-				$this->post = $this->items[1]->php;
-				?>
-
-			  	<article class="stream <?php echo $this->items[1]->platform; ?>">
-		  		<?php echo $this->loadTemplate($this->items[1]->platform); ?>
-		  		<time><?php echo $this->items[1]->date_created; ?></time>
-		  		</article>
-
-		</div>
-
-		<?php
-		/**
-		*
-		* Remove the first two items because we allready printed those.
-		*
-		*/
-		unset($this->items[0]);
-		unset($this->items[1]);
-		?>
-
-	<?php endif; ?>
 
 		<?php $count = 1; foreach($this->items as $item): ?>
 
-			<?php if ($count%4 == 1 && !$detect->isMobile()): ?>
+			<?php if ($count%2 == 1 && !$detect->isMobile()): ?>
 	        	<div>
 		    <?php endif; ?>
 
@@ -81,25 +41,23 @@ $detect = new Mobile_Detect;
 		<time><?php echo $item->date_created; ?></time>
 		</article>
 
-			<?php if ($count%4 == 0 && !$detect->isMobile()): ?>
+			<?php if ($count%2 == 0 && !$detect->isMobile()): ?>
 	        	</div>
 		    <?php endif; ?>
 		
 		<?php $count++; endforeach; ?>
 
-		<?php if ($count%4 != 1 && !$detect->isMobile()) echo "</div>"; ?>
+		<?php if ($count%2 != 1 && !$detect->isMobile()) echo "</div>"; ?>
 
   </div>
 </div>
 
-<?php if (!$detect->isMobile()): ?>
 <a class="next browse right">></a>
 <a class="prev browse left"><</a>
-<?php endif; ?>
 
 <script>
 if(jQuery().scrollable) {
-    $(".scrollable").scrollable();
+    $(".scrollable").scrollable({touch: false});
 }
 </script>
 
