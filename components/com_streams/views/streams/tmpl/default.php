5593645b77611ebe31f1dae3ac006be07747dcf0
Jean Brouwers <jeanbrouwers@outlook.com>
2013-10-16 12:23:13 +0200

    Created slider javascript

--- components/com_streams/views/streams/tmpl/default.php
+++ components/com_streams/views/streams/tmpl/default.php
@@ -32,7 +32,7 @@ $detect = new Mobile_Detect;
 					$this->post = $item->php;
 					?>
 
-					<article class="stream <?php echo $platform; ?>">
+					<article style="position: relative;" class="stream <?php echo $platform; ?>">
 					<?php echo $this->loadTemplate($platform); ?>
 					<time><?php echo $item->date_created; ?></time>
 					</article>
@@ -43,9 +43,39 @@ $detect = new Mobile_Detect;
 	</div>
 </div>
 
-<div class="pagination">
+<div class="pagination" style="display: none;">
 	<p class="counter pull-right">
 		<?php echo $this->pagination->getPagesCounter(); ?>
 		<?php echo $this->pagination->getPagesLinks(); ?>
 	</p>
 </div>
+
+<a class="left">left</a>
+<a class="right">right</a>
+
+<script>
+var count = 0;
+var items = 20;
+
+$(document).ready(function() {
+    $('a.left').click(function() {
+    	if (count > 0){
+			$('.stream').animate({
+           		left: '+=10%'
+        	}, 300);
+        	count--;
+        	console.log(count);
+        }
+    });
+
+    $('a.right').click(function() {
+    	if (count < (items - 2) / 2){
+			$('.stream').animate({
+           		left: '-=10%'
+        	}, 300);
+        	count++;
+        	console.log(count);
+        }
+    });
+});
+</script>
