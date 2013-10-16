c2fc35d7e6000162ed28ea096458d17bbf5af88a
JvG94 <jellevangrinsven@gmail.com>
2013-10-16 11:59:57 +0200

    Made new stylesheet for streams, changed default.php for a bit to make it work better

--- components/com_streams/views/streams/tmpl/default.php
+++ components/com_streams/views/streams/tmpl/default.php
@@ -12,84 +12,35 @@ require_once( JPATH_SITE.'/libraries/mobiledetect/Mobile_Detect.php' );
 $detect = new Mobile_Detect;
 ?>
 
-<style>
-article.stream
-{
-	width: 50%;
-	height: 300px;
-	float: left;
-}
+<div class="streamwrapper">
+	<div class="streamintro">
+		<p>
+			Hello, this is a small intro block.
+			I describe unnecessery info here.
+			And this is also not interesting.
+		</p>
+	</div>
+	<div class="itemswrapper">
+		<div class="items">
 
-article .intro
-{
-	width: 50%;
-	height: 300px;
-	float: left;
-}
+					<?php foreach($this->items as $item): ?>
 
-.items
-{
-	width: 50%;
-	overflow-x: scroll;
-	float: left;
-}
+				    <?php
+					$date = new JDate($item->date_created);
+					$item->date_created = $date->format(JText::_('DATE_FORMAT_LC2'));
+					$platform = $item->platform;
+					$this->post = $item->php;
+					?>
 
-.items div {
-	float: left;
-}
+					<article class="stream <?php echo $platform; ?>">
+					<?php echo $this->loadTemplate($platform); ?>
+					<time><?php echo $item->date_created; ?></time>
+					</article>
+					
+					<?php endforeach; ?>
 
-img
-{
-	width: 64px;
-	height: 64px;
-}
-
-.pagination
-{
-	display: none;
-}
-
-.intro
-{
-	width: 50%;
-}
-</style>
-
-<article class="stream intro">
-	<p>
-		Hello, this is a small intro block.
-		I describe unnecessery info here.
-		And this is also not interesting.
-	</p>
-</article>
-
-<div class="items">
-			<?php $count = 1; foreach($this->items as $item): ?>
-
-				<?php if ($count%2 == 1 && !$detect->isMobile()): ?>
-		        	<div>
-			    <?php endif; ?>
-
-		    <?php
-			$date = new JDate($item->date_created);
-			$item->date_created = $date->format(JText::_('DATE_FORMAT_LC2'));
-			$platform = $item->platform;
-			$this->post = $item->php;
-			?>
-
-			<article class="stream <?php echo $platform; ?>">
-			<?php echo $this->loadTemplate($platform); ?>
-			<time><?php echo $item->date_created; ?></time>
-			</article>
-
-				<?php if ($count%2 == 0 && !$detect->isMobile()): ?>
-		        	</div>
-			    <?php endif; ?>
-			
-			<?php $count++; endforeach; ?>
-
-			<?php if ($count%2 != 1 && !$detect->isMobile()) echo "</div>"; ?>
-
+		</div>
+	</div>
 </div>
 
 <div class="pagination">
