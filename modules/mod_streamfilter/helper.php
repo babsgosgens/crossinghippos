9caa0e3bf4513555f5bf4a1a7fed77389e155f7a
Jean Brouwers <jeanbrouwers@outlook.com>
2013-11-07 11:37:07 +0100

    Added mod-streamfilter module

---
+++ modules/mod_streamfilter/helper.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_streams
+ *
+ * @copyright   Copyright (C) 2013 Babs Gösgens
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Helper for mod_streams
+ *
+ * @package     Joomla.Site
+ * @subpackage  mod_streams
+ * @since       1.0
+ */
+class ModStreamfilterHelper
+{
+	public static function getPlatforms()
+	{
+		$db = JFactory::getDBO();
+		$db->setQuery('SELECT id, alias FROM #__streams_apis WHERE state = 1');
+		$result = $db->loadObjectList();
+		$platforms_list = array();
+
+		foreach ($result as $platform) {
+			array_push($platforms_list, $platform->alias);
+    	}
+
+    	return $platforms_list;
+	}
+}
