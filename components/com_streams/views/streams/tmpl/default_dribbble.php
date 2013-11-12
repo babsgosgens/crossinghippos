46c6e0ca71eb7d9ba4b38416f633590ad78728b9
JvG94 <jellevangrinsven@gmail.com>
2013-11-12 09:47:03 +0100

    Changed post index

--- components/com_streams/views/streams/tmpl/default_dribbble.php
+++ components/com_streams/views/streams/tmpl/default_dribbble.php
@@ -11,12 +11,10 @@ defined('_JEXEC') or die;
 
 // Uncomment for available attributes
 // echo '<pre>';
-//print_r($post);
+// print_r($post);
 // echo '</pre>';
 // exit;
 ?>
 
-	<p><img class="avatar" src="<?php echo $post->player->avatar_url; ?>" width="32" height="32">
 	<a href="http://dribbble.com/<?php echo $post->player->username; ?>"><span><?php echo $post->player->name;?></span></a></p>
-	<p><a href="<?php echo $post->url; ?>"><span><?php echo $post->title; ?></span></p>
-	<p><img class="postimage" src="<?php echo $post->image_url; ?>" width="150"></a></p>
+	<p><a href="<?php echo $post->url; ?>"> <img class="postimage" src="<?php echo $post->image_url; ?>" width="150"></a></p>
