1d12b12f91b4143e4ba67c62edac5a06af8c7140
babsgosgens <babsgosgens@gmail.com>
2014-01-06 01:29:24 +0100

    Add tracker

--- templates/crossinghippos/index.php
+++ templates/crossinghippos/index.php
@@ -188,5 +188,17 @@ include(JPATH_SITE.'/templates/'.$this->template.'/template/template.php');
 		});
 	});
 	</script>
+	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+		try {
+			var pageTracker = _gat._getTracker("UA-12737688-1");
+			pageTracker._setDomainName("none");
+			pageTracker._setAllowLinker(true);
+			pageTracker._trackPageview();
+		} catch(err) {}
+	</script>
 	</body>
 </html>
