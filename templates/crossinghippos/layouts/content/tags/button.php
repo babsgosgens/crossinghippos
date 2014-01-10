e843f6b47da3aa64f17a094dcb5e6b8d78450ca6
babsgosgens <babsgosgens@gmail.com>
2014-01-10 14:22:33 +0100

    There no longer seems to be an issue with id

--- templates/crossinghippos/layouts/content/tags/button.php
+++ templates/crossinghippos/layouts/content/tags/button.php
@@ -18,11 +18,12 @@ JLoader::register('TagsHelperRoute', JPATH_BASE . '/components/com_tags/helpers/
 			<?php
 			// Tags have id attr in com_tags and tag_id in com_content ?!? What's up with that ?!?
 			$id = isset($tag->tag_id) ? isset($tag->tag_id) : $tag->id ;
+			// var_dump($tag);
 			?>
 			<?php if (in_array($tag->access, JAccess::getAuthorisedViewLevels(JFactory::getUser()->get('id')))) : ?>
 				<?php $link_class = 'btn-base box--filled soft outline tags__anchor anchor anchor--incognito'; ?>
 					<li class="lt-base lt-gutters trailer">
-						<a href="<?php echo JRoute::_(TagsHelperRoute::getTagRoute($id . '-' . $tag->alias)) ?>" class="<?php echo $link_class; ?>" style="padding-right: .5em; padding-left: .5em;">
+						<a href="<?php echo JRoute::_(TagsHelperRoute::getTagRoute($tag->id . '-' . $tag->alias)) ?>" class="<?php echo $link_class; ?>" style="padding-right: .5em; padding-left: .5em;">
 						<i class="fa fa-tag"></i>&nbsp;<?php echo $this->escape($tag->title); ?><?php if(($i+1) < $tagsTotal): ?><?php endif; ?></a>
 					</li>
 
