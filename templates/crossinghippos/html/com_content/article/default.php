b030aa6414efeefb3488a9ef23c9cb89bfa56770
babsgosgens <babsgosgens@gmail.com>
2014-02-01 22:01:21 +0100

    Disqus prevents Bree from loading

--- templates/crossinghippos/html/com_content/article/default.php
+++ templates/crossinghippos/html/com_content/article/default.php
@@ -170,6 +170,20 @@ $categoryUrl = JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->catsl
 		echo $this->item->pagination;
 	}
 	?>
+
+    <script type="text/javascript">
+        var disqus_shortname = 'crossinghippos';
+	    var disqus_title = '<?php echo $this->escape($this->item->title); ?>';
+	    var disqus_identifier = '<?php echo $this->escape($this->item->slug); ?>';
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    <!--a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a-->
+
     
 
 	</div>
